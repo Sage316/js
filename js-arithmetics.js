ee0a4f90a5d6fc8075fe334f02bde05914f24c76
Sage316 <115668242+Sage316@users.noreply.github.com>
2023-01-27 15:17:55 +0200

    js-arithmetics

---
+++ js-arithmetics.js
@@ -0,0 +1,22 @@
+// TITLE
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const radius = 25 ** 2;
+const half = 0.5;
+let length = 10
+let breadth = 17
+let height = 9
+let base = 20
+const newBase = 20 * half 
+
+console.log("The area of the given circle is : ", radius * pi);
+
+console.log("\n");
+
+console.log("The area of the given rectangle is : ", length * breadth);
+
+console.log("\n");
+
+console.log("The area of the given triangle is : ", newBase * height );
